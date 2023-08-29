4d7defb1b1ccc16acba6ffcb65ddc9485fcabe70
sam-fsd <kabirusam037@gmail.com>
2023-08-29 10:28:00 -0700

    [fix] core dumped error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,6 +21,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (parent->left != NULL)
 	{
 		newnode->left = parent->left;
+		parent->left->parent = newnode;
 	}
 	else
 	{
